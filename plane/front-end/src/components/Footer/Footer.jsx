baf74d46a8c31e71232fe7bef4555052708cb07a
m3l1k4 <melikaihelas@gmail.com>
2020-07-10 12:13:42 -0700

    footer component being added

---
+++ plane/front-end/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.scss';
+
+export default function Footer() {
+    return (
+  <div>
+      
+  </div>
+    )
+}
