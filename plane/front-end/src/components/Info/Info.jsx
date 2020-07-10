4e6f63ada690c316f5e308f406766d54276236a0
m3l1k4 <melikaihelas@gmail.com>
2020-07-10 12:06:51 -0700

    styling in progress

--- plane/front-end/src/components/Info/Info.jsx
+++ plane/front-end/src/components/Info/Info.jsx
@@ -7,7 +7,18 @@ export default function Info() {
   <div className="Info">
   
   <div className="Info__body" >
-      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classic</p>
+      <p className="Info__body--text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classic</p>
+<img className="Info__body--img" src="../assets/browsing.jpg" alt="logo"/>
+  </div>
+
+  <div className="Info__body" >
+   <img className="Info__body--img" src="../assets/browsing.jpg" alt="logo"/> 
+     <p className="Info__body--text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classic</p>
+
+  </div>
+
+  <div className="Info__body" >
+      <p className="Info__body--text">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classic</p>
 <img className="Info__body--img" src="../assets/browsing.jpg" alt="logo"/>
   </div>
 
