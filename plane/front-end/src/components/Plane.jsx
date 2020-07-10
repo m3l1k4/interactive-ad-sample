4d6e525fc58cc1281ef306f5c7e528ddb4a0acfb
m3l1k4 <melikaihelas@gmail.com>
2020-07-09 23:51:24 -0700

    score calc in progress

--- plane/front-end/src/components/Plane.jsx
+++ plane/front-end/src/components/Plane.jsx
@@ -4,30 +4,64 @@ import './Plane.scss'
 
 
 
-export default function Plane({ handleHover }) {
+export default function Plane({ handleHover }, { handleScore }) {
 
     handleHover = (event) => { console.log("potato") }
+   
 
     return (
 
         <div>
             <h1>hello</h1>
 
-            <div className="sky">
-                <div className="sky__good-cloud"> 
-                <img className="sky__cloud-img"src='../assets/gcloud.png' alt="cloud"></img>
-          
-                </div>  
-                <div className="sky__bad-cloud">
-                                    <img onMouseOver={handleHover} className="sky__cloud-img"src='../assets/thunder.png' alt="cloud"></img>
+            <div onMouseOver={scoreCalc} className="sky">
+
+                <div className="score">
+                    <p>{scoreCalc}</p>
                 </div>
-    
+                <div className="sky__cloud  sky__goodA">
+                    <img className="sky__cloud-img" src='../assets/gcloud.png' alt="cloud"></img>
+
+                </div>
+                <div className="sky__cloud  sky__badA">
+                    <img onMouseOver={handleHover} className="sky__cloud-img" src='../assets/thunder.png' alt="cloud"></img>
+                </div>
+
+
+                <div className="sky__cloud  sky__goodB">
+                    <img className="sky__cloud-img" src='../assets/gcloud.png' alt="cloud"></img>
+
+                </div>
+                <div className="sky__cloud  sky__badB">
+                    <img onMouseOver={handleHover} className="sky__cloud-img" src='../assets/thunder.png' alt="cloud"></img>
+                </div>
+
+                <div className="sky__cloud  sky__goodC">
+                    <img className="sky__cloud-img" src='../assets/gcloud.png' alt="cloud"></img>
+
+                </div>
+                <div className="sky__cloud  sky__badC">
+                    <img onMouseOver={handleHover} className="sky__cloud-img" src='../assets/thunder.png' alt="cloud"></img>
+                </div>
+
+
+
+
+
             </div>
 
-           
+
 
         </div>
 
     )
+
+}
+
+export function scoreCalc(){
+
+    let score= String(new Date().getTime());
+    console.log(score)
+    return score;
 
 }
