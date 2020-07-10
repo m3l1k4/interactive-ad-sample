cab9eecfc6782343de556a4f5f0fd556081d07a2
m3l1k4 <melikaihelas@gmail.com>
2020-07-10 09:42:59 -0700

    components in progress

--- plane/front-end/src/App.js
+++ plane/front-end/src/App.js
@@ -4,8 +4,30 @@ import { Route, Redirect, Switch, BrowserRouter as Router } from 'react-router-d
 import './App.css';
 
 import Plane from './components/Plane'
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
+
+
+  state ={
+    score:[]
+  }
+
+
+
+//   handleScore = (event) => {
+
+
+//     this.setState({
+
+//         }]
+//     })
+   
+
+
+// }
+
+
 
   render() {
 
@@ -13,11 +35,11 @@ class App extends React.Component {
 
       <Router>
         <div className="App">
-
+        <NavBar />
           <Switch>
 
             <Route exact path="/">
-              <Plane />
+              {/* <Plane /> */}
             </Route>
 
           </Switch>
---
+++ plane/front-end/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {  Link } from 'react-router-dom'
+// import './NavBar.scss'
+
+
+
+
+export default function NavBar({ handleSubmit }) {
+    return (
+        <div className="NavBar">
+          
+
+
+        </div>
+    )
+}
+
+
