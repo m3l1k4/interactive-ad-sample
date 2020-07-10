7ebc282fb12c6ffc572c63eb96d389c0923759a3
m3l1k4 <melikaihelas@gmail.com>
2020-07-10 10:48:32 -0700

    nav bar logo added

--- plane/front-end/src/Home.js
+++ plane/front-end/src/Home.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Route, Redirect, Switch, BrowserRouter as Router } from 'react-router-dom'
 
 class App extends React.Component {
-    render() {
-      return (
-  
-       
-        <div className="App">
-<h1>home</h1>
-        </div>
-     
-      );
-    }
-  
+  render() {
+    return (
+
+
+      <div className="App">
+        <h1>home</h1>
+      </div>
+
+    );
   }
-  
-  export default App;
+
+}
+
+export default App;
--- plane/front-end/src/components/Nav/Nav.jsx
+++ plane/front-end/src/components/Nav/Nav.jsx
@@ -6,8 +6,8 @@ export default function Nav() {
     return (
         <nav className="nav">
             <Link className="nav__title-group" to="/">
-                <img className="nav__img" src={`${process.env.PUBLIC_URL}/assets/gcloud.png`} alt="Site logo" />
-                <h2 className="nav__title">Game of Games</h2>
+                <img className="nav__img" src={`${process.env.PUBLIC_URL}/assets/logo.png`} alt="Site logo" />
+                {/* <h2 className="nav__title">Game of Games</h2> */}
             </Link>
             <ul className="nav__list">
                 <Link to="/data" ><li className="nav__item">The Data</li></Link>
